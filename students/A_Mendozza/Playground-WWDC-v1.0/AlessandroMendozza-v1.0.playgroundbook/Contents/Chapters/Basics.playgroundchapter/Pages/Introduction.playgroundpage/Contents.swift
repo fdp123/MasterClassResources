3989c198f8b1874bcf11de66ca8d0dd146f1bb5e
alemens <alessandromendozza@icloud.com>
2018-05-30 01:56:23 +0200

    Add Playground-WWDC-v1.0

---
+++ students/A_Mendozza/Playground-WWDC-v1.0/AlessandroMendozza-v1.0.playgroundbook/Contents/Chapters/Basics.playgroundchapter/Pages/Introduction.playgroundpage/Contents.swift
@@ -0,0 +1,7 @@
+//: # Basics
+//:
+//: * [Connecting Nodes](Connecting%20Nodes)
+//: * [Mixing Nodes](Mixing%20Nodes)
+//: * [Splitting Nodes](Splitting%20Nodes)
+//: * [Dry Wet Mixer](Dry%20Wet%20Mixer)
+//:
