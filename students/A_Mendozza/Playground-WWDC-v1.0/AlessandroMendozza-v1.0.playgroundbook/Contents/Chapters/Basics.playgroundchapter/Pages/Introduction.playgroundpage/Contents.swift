b8861929a0cacb15ff4fc757bef4edc9585a4d61
Alemens <am@its.cx>
2021-01-11 19:48:53 +0100

    Up

--- students/A_Mendozza/Playground-WWDC-v1.0/AlessandroMendozza-v1.0.playgroundbook/Contents/Chapters/Basics.playgroundchapter/Pages/Introduction.playgroundpage/Contents.swift
+++
@@ -1,7 +0,0 @@
-//: # Basics
-//:
-//: * [Connecting Nodes](Connecting%20Nodes)
-//: * [Mixing Nodes](Mixing%20Nodes)
-//: * [Splitting Nodes](Splitting%20Nodes)
-//: * [Dry Wet Mixer](Dry%20Wet%20Mixer)
-//:
